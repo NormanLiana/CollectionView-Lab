4f013bcfb69c04abd5350e3c42c877f239294182
NormanLiana <liananorman@pursuit.org>
2019-09-29 21:40:23 -0400

    adds image view and 3 labels as outlets to DVC

--- Collection-View-Lab/Controllers/DetailCountryViewController.swift
+++ Collection-View-Lab/Controllers/DetailCountryViewController.swift
@@ -10,12 +10,20 @@ import UIKit
 
 class DetailCountryViewController: UIViewController {
 
+    // MARK: Outlets
+    @IBOutlet weak var imageOultet: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var capitalLabel: UILabel!
+    @IBOutlet weak var populationLabel: UILabel!
+    
+    // MARK: Properties
     var country: Country!
     
+    // MARK: Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-
-    
+    // MARK: Private Methods
+    }
 }
